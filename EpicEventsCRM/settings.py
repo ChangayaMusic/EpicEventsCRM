b55c1bac21ccc404f0af036c63d7cd772b93c368
ChangayaMusic <121641916+ChangayaMusic@users.noreply.github.com>
2024-02-25 16:57:27 +0100

    .enc created , secretkey and db login info off from setting to it

--- EpicEventsCRM/settings.py
+++ EpicEventsCRM/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gc%yfn8tql3nc#nz#%-_9=wvyys^pvm&*^av5n1z+s&qee3*fv'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -91,11 +91,11 @@ WSGI_APPLICATION = 'EpicEventsCRM.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ee_crm',
-        'USER': 'root',
-        'PASSWORD': 'Y@mahatdr125',
-        'HOST': 'localhost',   
-        'PORT': '3306',       
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
